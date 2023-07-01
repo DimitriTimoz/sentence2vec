a3f2cb8e21b10fff866552fdb23b25709460125b
DimitriTimoz <dimitri.timoz@orange.fr>
2023-07-01 23:15:48 +0200

    async Sentence2Vec

--- src/sentence2vec.rs
+++ src/sentence2vec.rs
@@ -12,7 +12,7 @@ impl<const D: usize> Sentence2Vec<D> {
     }
 
     /// Returns the vector representation of the sentence by averaging the vectors of the words in the sentence.
-    pub fn get_vec(&self, sentence: &str) -> Option<WordVec<D>> {
+    pub async fn get_vec(&self, sentence: &str) -> Option<WordVec<D>> {
         let sentence = sentence.to_lowercase();
         // Trim punctuation
         let sentence = sentence.trim_matches(|c: char| !c.is_alphanumeric());
@@ -43,8 +43,8 @@ impl<const D: usize> Sentence2Vec<D> {
         }
     }
 
-    pub fn cosine(&self, sentence1: &str, sentence2: &str) -> Option<f32> {
-        if let (Some(vec1), Some(vec2)) = (self.get_vec(sentence1), self.get_vec(sentence2)) {
+    pub async fn cosine(&self, sentence1: &str, sentence2: &str) -> Option<f32> {
+        if let (Some(vec1), Some(vec2)) = (self.get_vec(sentence1).await, self.get_vec(sentence2).await) {
             Some(vec1.cosine(&vec2))
         } else {
             None
